7c6f14827b5e34a205aa1b74d0e158a3c1e8f1aa
John McAlister <johmcali@amazon.com>
2018-08-12 09:31:20 +0100

    Lifting state up to its parent, converting Square to a functional component

--- src/index.js
+++ src/index.js
@@ -19,8 +19,24 @@ class Game extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />
+    );
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   render() {
@@ -49,22 +65,12 @@ class Board extends React.Component {
   }
 }
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({valueX: 'X', valueO: 'O'})}>
-        {this.state.valueX}
-      </button>
-    );
-  }
-
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 // ========================================
